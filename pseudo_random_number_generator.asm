4c18c485909a0ef1ddaa535fc09138eeb928da06
Fellasfaw <fellasfaw@outlook.com>
2023-08-01 17:18:04 +0300

    Added pseudo random number generator.

---
+++ pseudo_random_number_generator.asm
@@ -0,0 +1,63 @@
+.MODEL small
+.STACK 100h
+
+.DATA  
+    seed DW 0
+    random_number DW 0
+    ARBITRARY_NUM_1 equ 0x6C07h
+    ARBITRARY_NUM_2 equ 0xAAA5h
+
+.CODE
+    main PROC
+        MOV AX, @DATA
+        MOV DS, AX
+
+        CALL InitializeRandom  
+
+        CALL GenerateRandomNumber  
+
+        MOV AH, 4Ch
+        INT 21h
+
+    main ENDP
+
+
+    InitializeRandom PROC  ; Initializing the random number generator with the system time as the seed value
+        MOV AH, 2Ch  ; Getting system time
+        INT 21h
+
+        ; The system time is returned in CH (hours), CL (minutes), DH (seconds), and DL (milliseconds) and we combine them by shifting to set the seed value
+
+        MOV AX, CX
+        SHL AX, 8           
+        ADD AX, CX          
+        SHL AX, 8          
+        ADD AX, DX         
+        SHL AX, 8          
+        ADD AX, DX        
+
+        MOV seed, AX
+
+        RET
+    InitializeRandom ENDP
+
+
+    GenerateRandomNumber PROC  ; Generating the random 4-digit number using XORshift algorithm
+        MOV AX, seed    
+
+        XOR AX, ARBITRARY_NUM_1     
+        SHL AX, 7           
+        XOR AX, ARBITRARY_NUM_2    
+        SHR AX, 7          
+
+        ; Dividing the remainder by 9000 to obtain a 4-digit random number between 1000 and 9999
+        MOV BX, 9000
+        DIV BX  ; DX:AX = DX:AX / BX (remainder will be stored in AX)
+
+        ADD AX, 1000  ; AddING 1000 to the remainder to get a number between 1000 and 9999
+        MOV random_number, AX
+
+        RET
+    GenerateRandomNumber ENDP
+
+END main
